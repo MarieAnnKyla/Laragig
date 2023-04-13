20646e2ff0b382c819a2d472b07bd399af8006b4
JanHumphrey <sjanhumphrey@gmail.com>
2023-04-13 13:09:32 +0800

    Add flash-message alphine js

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -39,7 +39,9 @@ class ListingController extends Controller
         ]);
 
         Listing::create($formFields);
+
+       
         
-        return redirect('/');
+        return redirect('/')->with('message','Listing created successfully!');
       }
 }
