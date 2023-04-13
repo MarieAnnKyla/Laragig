cc9030caa5d56190ef38ac3b89ec037a7c0587e5
JanHumphrey <sjanhumphrey@gmail.com>
2023-04-14 00:24:58 +0800

    Add Auth for manage listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -58,12 +58,18 @@ class ListingController extends Controller
   //Show Edit Form
   public function edit(Listing $listing)
   {
-    return view('listings\edit', ['listing' => $listing]);
+    return view('listings/edit', ['listing' => $listing]);
   }
 
   //Update Listing Data
   public function update(Request $request, Listing $listing)
   {
+
+    //Make sure logged in user is owner
+    if($listing->user_id != auth()->id()){ 
+      abort(403,'Unauthorized Action');
+    }
+
     $formFields = $request->validate([
       'title' => 'required',
       'company' => ['required'],
@@ -88,6 +94,10 @@ class ListingController extends Controller
   //Delete Listing 
   public function destroy(Listing $listing)
   {
+    //Make sure logged in user is owner
+    if($listing->user_id != auth()->id()){ 
+      abort(403,'Unauthorized Action');
+    }
     $listing->delete();
     return redirect('/')->with('message', 'Listing deleted Successfully');
   }
